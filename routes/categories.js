1e97cbf186d9a87c53a447e0973bf54b76f36070
Rat1b <ratibor2007@yandex.ru>
2024-05-19 14:45:44 +0300

    U-UPDATE

--- routes/categories.js
+++ routes/categories.js
@@ -1,10 +1,17 @@
   const categoriesRouter = require('express').Router();
   
-  const {findAllCategories,createCategory,findCategoryById} = require('../middlewares/categories');
-  const {sendAllCategories,sendCategoryCreated,sendCategoryById} = require('../controllers/categories');
+  const {findAllCategories,createCategory,findCategoryById,updateCategory} = require('../middlewares/categories');
+  const {sendAllCategories,sendCategoryCreated,sendCategoryById,sendCategoryUpdated} = require('../controllers/categories');
 
   categoriesRouter.get('/categories', findAllCategories, sendAllCategories);
   categoriesRouter.post('/categories',findAllCategories,createCategory,sendCategoryCreated)
   categoriesRouter.get('/categories/:id',findCategoryById,sendCategoryById)
+
+  categoriesRouter.put(
+    "/categories/:id",
+    updateCategory,
+    sendCategoryUpdated
+  ); 
+
   module.exports = categoriesRouter;
   
--- routes/games.js
+++ routes/games.js
@@ -2,10 +2,19 @@
 
 const gamesRouter = require('express').Router();
 
-const {findAllGames,findGameById,createGame} = require('../middlewares/games');
-const {sendAllGames,sendGameCreated,sendGameById} = require('../controllers/games');
+const {findAllGames,findGameById,createGame,updateGame} = require('../middlewares/games');
+const {sendAllGames,sendGameCreated,sendGameById,sendGameUpdated} = require('../controllers/games');
 
 gamesRouter.get('/games', findAllGames, sendAllGames);
 gamesRouter.post("/games",findAllGames,createGame,sendGameCreated);
 gamesRouter.get("/games/:id",findGameById, sendGameById);
+
+gamesRouter.put(
+    "/games/:id", 
+      findGameById,
+      
+      updateGame,
+      sendGameUpdated
+  ); 
+
 module.exports = gamesRouter; 
