9a1fe9fe5fb1815c4eb5809adc947990ad69e0f0
Rat1b <ratibor2007@yandex.ru>
2024-05-19 13:22:16 +0300

    C-create
    закончил

--- middlewares/categories.js
+++ middlewares/categories.js
@@ -4,5 +4,19 @@ const findAllCategories = async (req, res, next) => {
   req.categoriesArray = await categories.find({});
   next();
 }
+const createCategory = async (req,res,next) =>{
+  console.log("POST /categories");
+  try {
+    console.log(req.body);
+    req.category  = await games.create(req.body);
+    next();
+  } catch (error) {
+      res.setHeader("Content-Type", "application/json");
+        res.status(400).send(JSON.stringify({ message: "Ошибка создания категории" }));
+  }
+}
 
-module.exports = findAllCategories; 
+
+module.exports = {findAllCategories,
+  createCategory
+}; 
--- middlewares/users.js
+++ middlewares/users.js
@@ -4,5 +4,17 @@ const findAllUsers = async (req, res, next) => {
   req.usersArray = await users.find({});
   next();
 }
-
-module.exports = findAllUsers; 
+const createUser = async (req,res,next) =>{
+  console.log("POST /categories");
+  try {
+    console.log(req.body);
+    req.user = await games.create(req.body);
+    next();
+  } catch (error) {
+      res.setHeader("Content-Type", "application/json");
+        res.status(400).send(JSON.stringify({ message: "Ошибка создания пользователя" }));
+  }
+}
+module.exports = {findAllUsers,
+  createUser
+}; 
