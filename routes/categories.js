531384e1eac421f0014f608ec90fba5a7e61dabd
Rat1b <ratibor2007@yandex.ru>
2024-05-26 10:21:46 +0300

    node_modules be gone!

--- routes/categories.js
+++ routes/categories.js
@@ -3,7 +3,13 @@
   const {findAllCategories,createCategory,findCategoryById,updateCategory,deleteCategory,checkIsCategoryExists,checkEmptyName} = require('../middlewares/categories');
   const {sendAllCategories,sendCategoryCreated,sendCategoryById,sendCategoryUpdated,sendCategoryDeleted} = require('../controllers/categories');
   const { checkAuth } = require("../middlewares/auth.js");
-  categoriesRouter.get('/categories', findAllCategories, sendAllCategories);
+  
+  categoriesRouter.get(
+    '/categories',
+    findAllCategories,
+     sendAllCategories
+    );
+
   categoriesRouter.post(
     "/categories",
     findAllCategories,
