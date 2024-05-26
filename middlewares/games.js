483a343ff6b953b2fd3ff8a7da2023b9d7164fba
Rat1b <ratibor2007@yandex.ru>
2024-05-26 11:39:15 +0300

    test

--- middlewares/games.js
+++ middlewares/games.js
@@ -89,7 +89,7 @@ const checkIfCategoriesAvaliable = async (req, res, next) => {
   }
 if (!req.body.categories || req.body.categories.length === 0) {
   res.setHeader("Content-Type", "application/json");
-      res.status(400).send(JSON.stringify({ message: "Выбери хотя бы одну категорию" }));
+      res.status(400).send(JSON.stringify({ message: "Выбери хотя бы одну категорию." }));
 } else {
   next();
 }
