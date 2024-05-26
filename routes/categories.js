83bfa2b2fb32ea0cf9d089b0c8807f14ae675c2f
Rat1b <ratibor2007@yandex.ru>
2024-05-26 11:26:14 +0300

    checkIfCategoriesAvaliable

--- routes/categories.js
+++ routes/categories.js
@@ -3,7 +3,6 @@
   const {findAllCategories,createCategory,findCategoryById,updateCategory,deleteCategory,checkIsCategoryExists,checkEmptyName} = require('../middlewares/categories');
   const {sendAllCategories,sendCategoryCreated,sendCategoryById,sendCategoryUpdated,sendCategoryDeleted} = require('../controllers/categories');
   const { checkAuth } = require("../middlewares/auth.js");
-  
   categoriesRouter.get(
     '/categories',
     findAllCategories,
